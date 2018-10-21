30bf730d7e680442a47c1fb5a08e0209d8104ded
Anandha Sudhan <rasudhan@gmail.com>
2018-10-21 22:07:58 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,22 @@
+create table projects(
+id INTEGER PRIMARY KEY,
+title text,
+category text,
+funding_goal integer,
+start_date text,
+end_date text
+);
+
+create table users(
+id INTEGER PRIMARY KEY,
+name text,
+age INTEGER
+
+);
+
+create table pledges(
+id INTEGER PRIMARY KEY,
+amount INTEGER,
+user_id INTEGER,
+project_id INTEGER
+);
